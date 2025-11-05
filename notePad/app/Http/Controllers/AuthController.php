5d727144f172f7922f3dc9e9aaf14eb9bf22cba5
dev-thiagorosario <eng.thiagorosario@icloud.com>
2025-11-05 08:05:41 -0300

    Refactor authentication logic, update user seeder, and create home and top bar views

--- notePad/app/Http/Controllers/AuthController.php
+++ notePad/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -19,29 +20,29 @@ class AuthController extends Controller
         ],
 
         [
-            'text_username'.'required' => 'O campo usuário é obrigatório.',
-            'text_username'.'email' => 'O campo usuário deve ser um email válido.',
-            'text_password'.'required' => 'O campo senha é obrigatório.',
-            'text_password'.'min' => 'O campo senha deve ter no mínimo 6 caracteres.'
+            'text_username.required' => 'O campo usuário é obrigatório.',
+            'text_username.email' => 'O campo usuário deve ser um email válido.',
+            'text_password.required' => 'O campo senha é obrigatório.',
+            'text_password.min' => 'O campo senha deve ter no mínimo 6 caracteres.'
         ]
-        
+
         );
     
         $username = $request->input('text_username');
         $password = $request->input('text_password');
         
-        $user = User::where('username', $username)-> where('deleted_at', NULL)->first();
+        $user = User::where('email', $username)-> where('deleted_at', NULL)->first();
 
         if(!$user){
-            return redirect()->back()->withInput()->with('Login error', 'Username ou Senha estão incorretos'); 
+            return redirect()->back()->withInput()->with('loginError', 'Email ou Senha estão incorretos'); 
         };
 
-        if(!password_verify($password, $user->password)){
-            return redirect()->back()->withInput()->with('Login error', 'Username ou Senha estão incorretos');
+        if(!Hash::check($password, $user->password)){
+            return redirect()->back()->withInput()->with('loginError', 'Email ou Senha estão incorretos');
         }
 
-    
-        $user->last_login = date('Y-m-d H-i-s');
+        // registra último acesso com precisão de segundos
+        $user->last_login_at = now();
         $user->save();
 
         session([
@@ -50,6 +51,8 @@ class AuthController extends Controller
                 'username' => $user->username
             ]
         ]);
+
+        return redirect()->to('/');
 
     }
 
--- notePad/database/seeders/UsersTableSeeder.php
+++ notePad/database/seeders/UsersTableSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,22 +15,32 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            [
-            'username' => 'user1@gmail.com',
-            'password' => bcrypt('abc123'),
-            'created_at' => date('Y-m-d H:i:s')
+           [
+                'username'        => 'user1',
+                'email'           => 'user1@gmail.com',
+                'password'        => Hash::make('senha123'),
+                'remember_token'  => Str::random(10),   
+                'created_at'      => now(),
+                'updated_at'      => now(),
             ],
             [
-            'username' => 'user41@gmail.com',
-            'password' => bcrypt('abc123'),
-            'created_at' => date('Y-m-d H:i:s')
+                'username'        => 'user41',
+                'email'           => 'user41@gmail.com',
+                'password'        => Hash::make('senha123'),
+                'remember_token'  => Str::random(10),
+                'created_at'      => now(),
+                'updated_at'      => now(),
             ],
             [
-            'username' => 'use31@gmail.com',
-            'password' => bcrypt('abc123'),
-            'created_at' => date('Y-m-d H:i:s')
-            ]
+                'username'        => 'user31',
+                'email'           => 'user31@gmail.com',
+                'password'        => Hash::make('senha123'),
+                'remember_token'  => Str::random(10),
+                'created_at'      => now(),
+                'updated_at'      => now(),
+            ],
 
         ]);
     }
 }
+
